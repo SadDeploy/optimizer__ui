0305df8fd0f75e1e45ec62ca59717d187d6701e7
ViralL <viirall@yandex.ru>
2015-11-04 21:59:22 +0300

    done pro

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,68 @@
 'use strict';
 
-var requireDir = require('require-dir');
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
 
-// handle enviromental variables in .env file
-var dotenv = require('dotenv');
-dotenv.load();
 
-// Require all tasks in gulp/tasks, including subfolders
-requireDir('./gulp/tasks', { recurse: true });
+// concat and minify css
+gulp.task('styles', function () {
+    return gulp.src('app/styles/**/*.css')
+        .pipe($.concatCss("main.css"))
+        .pipe(gulp.dest('dist/styles/'))
+        .pipe($.cssmin())
+        .pipe($.rename({
+            basename: "main",
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('dist/styles/'));
+});
+
+// concat and minify js
+gulp.task('script', function () {
+    return gulp.src('app/scripts/**/*.js')
+        .pipe($.concat('main.js'))
+        .pipe(gulp.dest('dist/scripts/'))
+        .pipe($.uglify())
+        .pipe($.rename({
+            basename: "main",
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('dist/scripts/'));
+});
+
+// minify html
+gulp.task('html', function () {
+    return gulp.src('app/*.html')
+        .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
+        .pipe(gulp.dest('dist'));
+});
+
+// optimize img
+gulp.task('images', function () {
+    return gulp.src('app/images/**/*')
+        .pipe($.cache($.imagemin({
+            progressive: true,
+            interlaced: true,
+            svgoPlugins: [{cleanupIDs: false}]
+        })))
+        .pipe(gulp.dest('dist/images'));
+});
+
+// copy independent files
+gulp.task('files', function () {
+    return gulp.src('app/*')
+        .pipe(gulp.dest('dist/'));
+});
+
+// clean directory
+gulp.task('clean', require('del').bind(null, ['dist']));
+
+// default task
+gulp.task('default', ['styles', 'script', 'html', 'images', 'files'], function () {
+    return gulp.src('dist/**/*').pipe($.size({title: 'Optimize', gzip: true}));
+});
+
+// optimize task
+gulp.task('optimize', ['clean'], function () {
+    gulp.start('default');
+});
--- readme.md
+++ readme.md
@@ -15,9 +15,15 @@ Installation process:
 
 ## Usage
 
+1. Copy all ccs  into "app/styles" folder
+2. Copy all  js  into "app/scripts" folder
+3. Copy all img  into "app/images" folder
+4. Copy all html into "app/" folder
 
+	THEN
+	
 To optimize project run:
 ```
-gulp optimizer [--force] 
+gulp optimize [--force] 
 ```
 
